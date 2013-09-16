1e78638d2a1229b4383fd09309145defe3eaf65e
Jesse Yang <jyyjcc@gmail.com>
2013-09-16 15:06:16 +0800

    fix

--- david/modules/works/view.py
+++ david/modules/works/view.py
@@ -21,6 +21,7 @@ def show(uid):
     carousel_items = [dict(img=a.url(), caption=a.desc) for a in work.attachment_pics()]
     cat = work.cat_name
     artist = work.artist
+    artist_work_url = artist_work_url_func(work.cat_name)
     return st('/modules/works/%s/show.html' % cat, **locals())
 
 
@@ -39,10 +40,11 @@ def list(catname, page=1):
     page = int(page)
     perpage = 100
 
+    artist_work_url = artist_work_url_func(catname)
+    
     artist_id = request.args.get('artist', '')
     artist_id = int(artist_id) if artist_id.isdigit() else None
     artist = Artist.get(artist_id)
-
     query = Work.query.filter(Work.cat == cat_id).order_by(Work.pubdate.desc())
     if artist_id:
         query = query.filter(Work.artist_id == artist_id)
@@ -59,5 +61,15 @@ def list(catname, page=1):
     years = sorted(set(x.pubdate.year for x in items), reverse=True)
 
     return st('/modules/works/%s/index.html' % catname, **locals())
+
+
+
+
+def artist_work_url_func(catname):
+    def func(artist):
+        args = dict(catname=catname)
+        args['artist'] = artist.id
+        return url_for('works.list', **args)
+    return func
 
 __all__ = [bp]
